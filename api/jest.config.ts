2d351f2fcab4b3ea87a296c89e00645e8cda563f
bisk8s <bisk8scode@gmail.com>
2021-02-26 14:55:10 -0300

    jester support api

---
+++ api/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+
+const { compilerOptions } = require('./tsconfig.paths.json');
+
+const config: Config.InitialOptions = {
+  bail: 1,
+  verbose: true,
+  clearMocks: true,
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+};
+export default config;
