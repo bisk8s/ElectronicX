395d50812841cdcaca228f540b1de233ff676c9d
bisk8s <bisk8scode@gmail.com>
2021-02-26 05:08:28 -0300

    generate jwt

--- api/src/modules/Users/controllers/AuthController.ts
+++ api/src/modules/Users/controllers/AuthController.ts
@@ -1,9 +1,11 @@
 import { getRepository, Repository } from 'typeorm';
 import { Request, Response, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
 
 import User from '@entities/User';
 
 let ormRepository: Repository<User>;
+const { APP_SECRET } = process.env;
 
 export default class AuthController {
   startRepository(_rq:Request, _rs:Response, next:NextFunction) {
@@ -16,10 +18,13 @@ export default class AuthController {
   async authenticate(request: Request, response:Response) {
     try {
       const { username, password } = request.body;
-      const user = await ormRepository.findOne({ where: { username }, select: ['password'] });
+      const user = await ormRepository.findOne({ where: { username }, select: ['id', 'password'] });
 
       const sucess = await user.comparePassword.bind(user)(password);
-      response.send({ sucess });
+      const token = jwt.sign({ id: user.id }, APP_SECRET, { expiresIn: 86400 });
+      if (sucess) {
+        response.send({ sucess, token });
+      }
     } catch (error) {
       response.status(422).send(error);
     }
