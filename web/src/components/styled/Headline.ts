57b1a270eb2abd9c6e539ad2882f78068fe8b8ad
bisk8s <bisk8scode@gmail.com>
2021-02-27 01:25:19 -0300

    components added

---
+++ web/src/components/styled/Headline.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Headline = styled.h1`
+    font-family: 'Boltz';
+    font-weight: normal;
+    color: #FFFFFF
+`;
+
+export default Headline;
---
+++ web/src/components/styled/Wrapper.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    background-color: #333333AA;
+`;
+
+export default Wrapper;
