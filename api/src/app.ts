f719bfbdeae1010c99143bc624dfb2f2b81f49e3
bisk8s <bisk8scode@gmail.com>
2021-02-27 18:31:49 -0300

    added cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import rootRoutes from '@routes/Root';
 
 const app = express();
 
 // add rules
+app.use(cors());
+
 app.use(express.json());
 app.use(rootRoutes);
 
